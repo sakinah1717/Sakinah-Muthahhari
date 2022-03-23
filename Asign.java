f3f82de62f107b57ea56b9ed454239cf70224807
sakinah1717 <102119627+sakinah1717@users.noreply.github.com>
2022-03-23 03:58:47 -0700

    Add files via upload

---
+++ Asign.java
@@ -0,0 +1,19 @@
+/* Nama                     : SAKINAH MUTHAHHARI */
+/* NIM                      : 13020200292 */
+/* HARI/ TANGGAL DAN WAKTU  : MINGGU/ 20 MARET 11:06 */
+/* KELAS                    : B3 */
+
+public class Asign {
+
+       /**
+        * @param args
+        */
+       public static void main(String[] args) {
+            // TODO Auto-generated method stub
+           /* Kamus */
+                 int i;
+          /* Program */
+                System.out.print ("hello\n"); i = 5;
+                System.out.println ("Ini nilai i :" + i); 
+      }
+   }
